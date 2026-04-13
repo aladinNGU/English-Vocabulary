7dfb3106763a69f8cd34011e893bbe942ce21bee
alauddin <alauddinkmj@gmail.com>
2026-04-13 23:30:30 +0600

    vocabulary2

--- script.js
+++ script.js
@@ -16,12 +16,23 @@ const displayWord = (words) => {
   console.log(words);
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `<div class="text-center bg-sky-100 col-span-full space-y-2 font-bangla py-10">
+    <img class="mx-auto" src="./assets/alert-error.png"/>
+      <p class="text-xl text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="text-3xl font-bold">নেক্সট Lesson এ যান</h2>
+    </div>`;
+
+    return;
+  }
+
   words.forEach((word) => {
     const wordCard = document.createElement("div");
     wordCard.innerHTML = ` <div class="bg-white rounded-xl shadow-sm text-center py-15 px-5 space-y-3">
-        <h3 class="text-xl font-bold">${word.word}</h3>
+        <h3 class="text-xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h3>
         <p>Meaning/Pronounciation</p>
-        <div class="text-xl font-bold font-bangla">"${word.meaning}/${word.pronunciation}"</div>
+        <div class="text-xl font-bold font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}/${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
         <div class="flex justify-between w-8/12 mx-auto">
             <button class="btn bg-[#1a90ff10] hover:bg-[#1a90ff80]"><i class="fas fa-info-circle"></i></button>
             <button class="btn bg-[#1a90ff10] hover:bg-[#1a90ff80]"><i class="fas fa-volume-up"></i></button>
@@ -41,7 +52,7 @@ const displayLesson = (lessons) => {
     // 3. create the Element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary"> <i class="fas fa-book-open"></i> Lesson - ${lesson.level_no} </button>
+    <button id="lesson-btn-${lesson.level_no}" onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn "> <i class="fas fa-book-open"></i> Lesson - ${lesson.level_no} </button>
   `;
     // 4. append the element to level container
     levelContainer.append(btnDiv);
---
+++ substract.py
@@ -0,0 +1,4 @@
+number1=int(input('Enter First Integer Number: '))
+number2=int(input('Enter 2nd Integer Number: '))
+subtract = number1-number2;
+print('Result of subtraction from', number1, 'to', number2, '=', subtract) 
