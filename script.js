c1a18611699b25441009a94399d6fb7f8eb56135
alauddin <alauddinkmj@gmail.com>
2026-04-16 23:23:03 +0600

    vocabulary3

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join(" ");
+};
+
 const loadLessons = () => {
   const url = "https://openapi.programming-hero.com/api/levels/all";
 
@@ -6,12 +11,55 @@ const loadLessons = () => {
     .then((data) => displayLesson(data.data));
 };
 
+const removeActiveClass = () => {
+  const lessonBtns = document.querySelectorAll(".lesson-btn");
+  console.log(lessonBtns);
+  lessonBtns.forEach((btn) => btn.classList.remove("active"));
+};
+
 const loadWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayWord(data.data));
+    .then((data) => {
+      removeActiveClass(); // remove all active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      // console.log(clickBtn);
+      clickBtn.classList.add("active"); // add active class
+      displayWord(data.data);
+    });
 };
+
+const loadWordDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `<div>
+            <h2 class="text-2xl font-bold">
+             ${word.word}<i class="fa-solid fa-microphone-lines"></i> : ${word.pronunciation}
+            </h2>
+          </div>
+          <div>
+            <h2 class="font-bold">Meaning</h2>
+            <p>${word.meaning}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">Example</h2>
+            <p>${word.sentence}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">Synonym</h2>
+            <div>${createElements(word.synonyms)}</div>
+         </div>`;
+  document.getElementById("word_modal").showModal();
+};
+
 const displayWord = (words) => {
   console.log(words);
   const wordContainer = document.getElementById("word-container");
@@ -34,7 +82,7 @@ const displayWord = (words) => {
         <p>Meaning/Pronounciation</p>
         <div class="text-xl font-bold font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"}/${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
         <div class="flex justify-between w-8/12 mx-auto">
-            <button class="btn bg-[#1a90ff10] hover:bg-[#1a90ff80]"><i class="fas fa-info-circle"></i></button>
+            <button onclick="loadWordDetails(${word.id})" class="btn bg-[#1a90ff10] hover:bg-[#1a90ff80]"><i class="fas fa-info-circle"></i></button>
             <button class="btn bg-[#1a90ff10] hover:bg-[#1a90ff80]"><i class="fas fa-volume-up"></i></button>
         </div>
       </div>
@@ -43,7 +91,7 @@ const displayWord = (words) => {
   });
 };
 const displayLesson = (lessons) => {
-  console.log(lessons);
+  // console.log(lessons);
   // 1. get the level container
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
